a812668b074d9c21d23da535af4cacc83d99286f
dindy01 <127743461+dindy01@users.noreply.github.com>
2025-02-05 00:45:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("yesButton").addEventListener("click", function() {
     // Reindirizza alla pagina della cartolina
     window.location.href = "cartolina.html";  // Assicurati che cartolina.html sia nella stessa cartella
 });
+
+document.getElementById("noButton").addEventListener("click", function() {
+    //Reindirizza alla gif
+    window.location.href = "noreaction.html";
+})
