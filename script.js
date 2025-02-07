ea8a2e98ae1cec8141362d6e93dd978dfcfcc12c
dindy01 <dindiba2000@gmail.com>
2025-02-07 22:12:54 +0100

    aggironamento2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById("yesButton").addEventListener("click", function() {
     // Reindirizza alla pagina della cartolina
     window.location.href = "cartolina.html";  // Assicurati che cartolina.html sia nella stessa cartella
@@ -8,14 +7,3 @@ document.getElementById("noButton").addEventListener("click", function() {
     //Reindirizza alla gif
     window.location.href = "noreaction.html";
 })
-=======
-document.getElementById("yesButton").addEventListener("click", function() {
-    // Reindirizza alla pagina della cartolina
-    window.location.href = "cartolina.html";  // Assicurati che cartolina.html sia nella stessa cartella
-});
-
-document.getElementById("noButton").addEventListener("click", function() {
-    //Reindirizza alla gif
-    window.location.href = "noreaction.html";
-})
->>>>>>> 34a6991 (Aggiornamento 07/02/25)
